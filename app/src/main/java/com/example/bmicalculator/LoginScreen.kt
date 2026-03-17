dbbbd7c1c7123d56a170df417df9f937af2c152c
jeevanrt2807 <jeevanreddythummala8@gmail.com>
2026-03-18 00:58:22 +0530

    developed login and registration screens.

---
+++ app/src/main/java/com/example/bmicalculator/LoginScreen.kt
@@ -0,0 +1,13 @@
+package com.example.bmicalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
